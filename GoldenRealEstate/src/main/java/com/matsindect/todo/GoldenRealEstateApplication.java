2e43106d161609228de027e97a7d76467d12af42
matsindect <matsindect@gmail.com>
2020-06-16 21:57:34 +0400

    Initial commit

---
+++ GoldenRealEstate/src/main/java/com/matsindect/todo/GoldenRealEstateApplication.java
@@ -0,0 +1,13 @@
+package com.matsindect.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRealEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenRealEstateApplication.class, args);
+	}
+
+}
---
+++ GoldenRealEstate/src/test/java/com/matsindect/todo/GoldenRealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsindect.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRealEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
